73b2636d8f56f50371a7a19e855a6533a9ef8437
VikasBagri-Oodles <137488882+VikasBagri-Oodles@users.noreply.github.com>
2023-07-11 15:18:55 +0530

    Add files via upload
    Encoded Polyline is also implemented.

--- Vehicle-Routing-Problem/src/main/java/com/vehicle/routing/problem/VehicleRoutingProblemApplication.java
+++ Vehicle-Routing-Problem/src/main/java/com/vehicle/routing/problem/VehicleRoutingProblemApplication.java
@@ -1,5 +1,6 @@
 package com.vehicle.routing.problem;
 
+import com.google.maps.model.EncodedPolyline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VehicleRoutingProblemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(VehicleRoutingProblemApplication.class, args);
+
 	}
 
 }
--- Vehicle-Routing-Problem/src/main/java/com/vehicle/routing/problem/controller/Controller.java
+++ Vehicle-Routing-Problem/src/main/java/com/vehicle/routing/problem/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.vehicle.routing.problem.controller;
 
+import com.google.maps.model.EncodedPolyline;
+import com.google.maps.model.LatLng;
 import com.vehicle.routing.problem.domain.Location;
 import com.vehicle.routing.problem.domain.Vehicle;
 import com.vehicle.routing.problem.domain.VehicleRouting;
@@ -22,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -36,6 +39,8 @@ public class Controller {
     @Autowired
     private ShortestRouteService shortestRouteService;
 
+    // controller to get the shortest route
+    // responds back with a JSON data
     @PostMapping("/solve")
     public List<Location> solve(@RequestBody Coordinates coordinates) {
 
@@ -44,6 +49,28 @@ public class Controller {
 
         // optimize route using OptaPlanner
         return shortestRouteService.getShortestRoute(coordinates, distanceMatrix);
+
+    }
+
+    // controller to get the shortest route
+    // responds back with an Encoded Polyline of the shortest route
+    @PostMapping("/solve/polyline_encode")
+    public String polylineEncode(@RequestBody Coordinates coordinates) {
+
+        // construct a distance matrix using Distance Matrix API
+        Distance distanceMatrix = matrixService.getDistanceMatrix(coordinates);
+
+        // optimize route using OptaPlanner
+        List<Location> route = shortestRouteService.getShortestRoute(coordinates, distanceMatrix);
+
+        List<LatLng> latLng = new ArrayList<>();
+        route.forEach(location -> {
+            latLng.add(new LatLng(location.getLat(), location.getLon()));
+        });
+
+        EncodedPolyline encodedPolyline = new EncodedPolyline(latLng);
+
+        return encodedPolyline.getEncodedPath();
 
     }
 
